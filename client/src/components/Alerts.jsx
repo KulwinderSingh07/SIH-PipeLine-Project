b41cc40c2a2995534aef6d646688a9402e0c6cf0
viren1610 <virenplays@gmail.com>
2023-12-20 02:35:03 +0530

    Alerts page done, changes with theme color are imp

--- client/src/components/Alerts.jsx
+++ client/src/components/Alerts.jsx
@@ -1,30 +1,146 @@
 import React from 'react'
 import Header from './header'
 import Alert from '@mui/material/Alert';
-import { AlertTitle, Stack, Typography, Divider } from '@mui/material';
+import { AlertTitle, Stack, Typography, Divider, Box, IconButton } from '@mui/material';
+import { CircleDashed, } from "phosphor-react";
 
 const alerts = [
     {
         alertId: 101,
-        supervisorId: "AJ101"
+        supervisorId: "AJ101",
+        timeStance: "11:34"
     },
     {
         alertId: 102,
-        supervisorId: "PC102"
+        supervisorId: "PC102",
+        timeStance: "03:24"
     },
     {
         alertId: 103,
-        supervisorId: "BM103"
+        supervisorId: "BM103",
+        timeStance: "10:14"
     },
     {
         alertId: 104,
-        supervisorId: "VI104"
+        supervisorId: "VI104",
+        timeStance: "1:34"
     },
 ]
 
 const Alerts = ({ el }) => {
     return (
         <>
+               <Box mb="10px">
+    <Typography
+      variant="h3"
+      color="black"
+      fontWeight="bold"
+      sx={{ m: "0 0 4px 0" }}
+    >
+    Alerts
+    </Typography>
+    <Typography variant="h6" color="Black">
+   Provide with time to time alerts
+    </Typography>
+  </Box>
+            <Divider width={"20%"}/>
+            <Stack direction="row" spacing={3}>
+
+                
+                <Box sx={{
+                    position: "relative",
+                    width: "50%"
+
+                }}>
+                    <Stack p={3} spacing={2} sx={{ height: "100vh" }}>
+                        <Stack
+                            direction="row"
+                            alignItems={"center"}
+                            justifyContent={"space-between"}
+                        >
+                            <Typography variant="h4">Warnings</Typography>
+                        </Stack>
+                        <Divider width={"20%"} />
+                        <Typography variant='h5'> Water Theft Alerts</Typography>
+                        <Stack direction="column">
+                            {alerts.map((el) => (
+                                <Alert key={el.alertId} severity="error" sx={{ mb: 5, borderRadius: '15px' }}>
+                                    <AlertTitle>
+                                        <Stack
+                                            direction="row"
+                                            alignItems="center"
+                                            justifyContent="space-between"
+                                            spacing={6}
+                                        >
+                                            <Typography variant='body1'> Alert-Id {el.alertId} </Typography>
+                                            <Typography>Supervisor-Id {el.supervisorId}</Typography>
+                                            <Typography> Time-Instance{el.timeStance}</Typography>
+
+                                        </Stack>
+                                    </AlertTitle>
+                                    This is an Theft alert — <strong>check it out!</strong>
+                                </Alert>
+
+                            ))}
+                        </Stack>
+
+
+                    </Stack>
+
+                </Box>
+
+                <Box sx={{
+                    position: "relative",
+
+                }}>
+                    <Stack p={3} spacing={2} sx={{ height: "100vh" }}>
+                        <Stack
+                            direction="row"
+                            alignItems={"center"}
+                            justifyContent={"space-between"}
+                        >
+                            <Typography variant="h4">Emergency</Typography>
+                        </Stack>
+                        <Divider width={"35%"} />
+                        <Typography variant='h5'>Various Water Anomalies</Typography>
+                        <Stack direction="column">
+                            {alerts.map((el) => (
+                                <Alert key={el.alertId} severity="warning" sx={{ mb: 5, borderRadius: '15px' }}>
+                                    <AlertTitle>
+                                        <Stack
+                                            direction="row"
+                                            alignItems="center"
+                                            justifyContent="space-between"
+                                            spacing={6}
+                                        >
+                                            <Typography variant='body1'> Alert-Id {el.alertId} </Typography>
+                                            <Typography>Supervisor-Id {el.supervisorId}</Typography>
+                                            <Typography> Time-Instance{el.timeStance}</Typography>
+
+                                        </Stack>
+                                    </AlertTitle>
+                                    This is an Emergency alert — <strong>check it out!</strong>
+                                </Alert>
+
+                            ))}
+                        </Stack>
+
+
+                    </Stack>
+
+                </Box>
+            </Stack>
+
+
+
+
+
+
+
+
+
+
+            {/* <Stack></Stack> 
             <Typography
                 variant="h3"
                 color="black"
@@ -49,10 +165,11 @@ const Alerts = ({ el }) => {
                                 direction="row"
                                 alignItems="center"
                                 justifyContent="space-between"
-                                spacing={0.4}
+                                spacing={1}
                             >
                                 <Typography variant='body1'>Theft alert with an Alert-Id {el.alertId}</Typography>
                                 <Typography>with your notable Supervisor-Id{el.supervisorId}</Typography>
+                                <Typography>{el.timeStance}</Typography>
 
                             </Stack>
                         </AlertTitle>
@@ -85,7 +202,7 @@ const Alerts = ({ el }) => {
                     </Alert>
 
                 ))}
-            </Stack>
+            </Stack>*/}
 
 
         </>
